b2c74c45aa1854062c9ffd2a6a7136495d446659
ChenYing Kuo <evshary@gmail.com>
2020-08-22 11:51:36 +0800

    Add sleep to stabilze the camera.
    Easter Egg: The sleep time is adjustable. You can adjust it to have
    better performance.

--- src/snapshot_client.hpp
+++ src/snapshot_client.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <unistd.h> // Used by sleep
 #include "rclcpp/rclcpp.hpp"
 #include "std_srvs/srv/empty.hpp"
 #include <behaviortree_cpp_v3/action_node.h>
@@ -29,6 +30,7 @@ class SnapshotClient : public BT::SyncActionNode
                 }
                 RCLCPP_INFO(node_->get_logger(), "Waiting for service /save to appear...");
             }
+            sleep(3); // To stablize the camera
             auto request = std::make_shared<std_srvs::srv::Empty::Request>();
             auto result_future = client_->async_send_request(request);
             if (rclcpp::spin_until_future_complete(node_, result_future) != rclcpp::FutureReturnCode::SUCCESS)
@@ -36,6 +38,7 @@ class SnapshotClient : public BT::SyncActionNode
                 RCLCPP_ERROR(node_->get_logger(), "Unable to call /save");
                 return BT::NodeStatus::FAILURE;
             }
+            sleep(1); // To stablize the camera
             return BT::NodeStatus::SUCCESS;
         }
 
